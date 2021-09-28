1e7221f918fe5c0013fe23598be229b4f3514228
Tim Garrity <tag.garrity@gmail.com>
2021-09-28 11:22:39 -0400

    add new content type const

--- understanding_net_http_package/35_exploring_http_ResponseWriter/main.go
+++ understanding_net_http_package/35_exploring_http_ResponseWriter/main.go
@@ -6,15 +6,16 @@ import (
 )
 
 const (
-	contentType = "Content-Type"
-	contentHTML = "text/html; charset=UTF-8"
+	contentType  = "Content-Type"
+	contentHTML  = "text/html; charset=UTF-8"
+	contentPlain = "text/plain; charset=UTF-8"
 )
 
 type hotdog int
 
 func (h hotdog) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Tim", "sucks.")
-	w.Header().Set(contentType, contentHTML)
+	w.Header().Set(contentType, contentPlain)
 	fmt.Fprintln(w, "<h1>This is my code, please suck it</h1>")
 }
 
