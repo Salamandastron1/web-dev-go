018ebcc7a8d8633ffb616dfc5e03afc5d8dae873
Tim Garrity <tag.garrity@gmail.com>
2021-10-01 14:44:01 -0400

    DRY up code, optimize runtime with strings.Builder

--- understanding_net_http_package/40_hands_on_exercises/1/main.go
+++ understanding_net_http_package/40_hands_on_exercises/1/main.go
@@ -4,18 +4,22 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strings"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Wow looks like you requested the default route: %v\nFrom this host: %v", r.URL.Path, r.Host)
-	})
-	http.HandleFunc("/dog", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Wow looks like you requested the route: %v\nFrom this host %v", r.URL.Path, r.Host)
-	})
-	http.HandleFunc("/me", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Wow looks like you requested the route: %v\nFrom this host: %v\nBy the way, my name is Tim", r.URL.Path, r.Host)
-	})
+	http.HandleFunc("/", handle)
+	http.HandleFunc("/dog", handle)
+	http.HandleFunc("/me", handle)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func handle(w http.ResponseWriter, r *http.Request) {
+	var sb strings.Builder
+	sb.WriteString("Wow looks like you requested the route: %v\nFrom this host: %v\n")
+	if r.URL.Path == "/me" {
+		sb.WriteString("By the way, my name is Tim")
+	}
+	fmt.Fprintf(w, sb.String(), r.URL.Path, r.Host)
 }
