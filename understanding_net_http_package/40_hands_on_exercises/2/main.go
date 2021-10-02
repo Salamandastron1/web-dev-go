110bb0bdb53720c0a1db8fe1dc11d13899c7c7c1
Tim Garrity <tag.garrity@gmail.com>
2021-10-02 12:35:40 -0400

    error handling for edge cases

--- understanding_net_http_package/40_hands_on_exercises/2/main.go
+++ understanding_net_http_package/40_hands_on_exercises/2/main.go
@@ -25,10 +25,16 @@ func main() {
 func handle(w http.ResponseWriter, r *http.Request) {
 	var sb strings.Builder
 	var sl []string
-	sb.WriteString("Wow looks like you requested the route: %v\nFrom this host: %v\n")
+	_, err := sb.WriteString("Wow looks like you requested the route: %v\nFrom this host: %v\n")
+	if err != nil {
+		log.Fatalln(err)
+	}
 	if r.URL.Path == "/me" {
 		sb.WriteString("By the way, my name is Tim")
 	}
 	sl = strings.Split(fmt.Sprintf(sb.String(), r.URL.Path, r.Host), "\n")
-	tpl.ExecuteTemplate(w, "index.gohtml", sl)
+	err = tpl.ExecuteTemplate(w, "index.gohtml", sl)
+	if err != nil {
+		log.Fatalln("Error executing template", err)
+	}
 }
