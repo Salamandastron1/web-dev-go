8f35f807ff09abe4ba3da8329a628943d9373148
Tim Garrity <tag.garrity@gmail.com>
2021-10-28 21:58:44 -0400

    able to use context to store values function calls

--- web_dev_toolkit/105_context/main.go
+++ web_dev_toolkit/105_context/main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
 )
 
+type userID string
+type fname string
+
 func main() {
 	http.HandleFunc("/", foo)
+	http.HandleFunc("/bar", bar)
 	http.Handle("/favicon.ico", http.NotFoundHandler())
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
@@ -15,6 +20,20 @@ func main() {
 
 func foo(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
+	ctx = context.WithValue(ctx, userID("userID"), 777)
+	ctx = context.WithValue(ctx, fname("fname"), "Bond")
+
+	results := dbAccess(ctx)
+	fmt.Fprintln(w, results)
+}
+
+func bar(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
 	log.Println(ctx)
 	fmt.Fprintln(w, ctx)
+}
+
+func dbAccess(ctx context.Context) int {
+	return ctx.Value(userID("userID")).(int)
 }
