98e55f7ad060aea253eccea57a57353a3579623e
Tim Garrity <tag.garrity@gmail.com>
2021-10-28 22:12:14 -0400

    custom types for ctx, use withTimeout to cancel long running process

--- web_dev_toolkit/105_context/main.go
+++ web_dev_toolkit/105_context/main.go
@@ -5,10 +5,13 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 )
 
 type userID string
 type fname string
+
+var uID userID = "userID"
 
 func main() {
 	http.HandleFunc("/", foo)
@@ -20,10 +23,14 @@ func main() {
 
 func foo(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
-	ctx = context.WithValue(ctx, userID("userID"), 777)
+	ctx = context.WithValue(ctx, uID, 777)
 	ctx = context.WithValue(ctx, fname("fname"), "Bond")
 
-	results := dbAccess(ctx)
+	results, err := dbAccess(ctx)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusRequestTimeout)
+		return
+	}
 	fmt.Fprintln(w, results)
 }
 
@@ -34,6 +41,27 @@ func bar(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, ctx)
 }
 
-func dbAccess(ctx context.Context) int {
-	return ctx.Value(userID("userID")).(int)
+func dbAccess(ctx context.Context) (int, error) {
+	ctx, cancel := context.WithTimeout(ctx, 1*time.Second)
+	defer cancel()
+
+	ch := make(chan int)
+	go func() {
+		// ridiculous long running task
+		uid := ctx.Value(uID).(int)
+		time.Sleep(10 * time.Second)
+
+		//check to mae sure we're not running in vain
+		// if ctx.Done()
+		if ctx.Err() != nil {
+			return
+		}
+		ch <- uid
+	}()
+	select {
+	case <-ctx.Done():
+		return 0, ctx.Err()
+	case i := <-ch:
+		return i, nil
+	}
 }
