af4234c339aa9b98f6df93deeb3478a2929f400c
Tim Garrity <tag.garrity@gmail.com>
2021-11-07 17:46:30 -0500

    dial mongo session and pass to user struct

---
+++ go_and_mongodb/118_connect_to_mongodb/controllers/controllers.go
@@ -0,0 +1,67 @@
+package controllers
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"web-dev-go/go_and_mongodb/organizing_code_into_packages/models"
+
+	"github.com/julienschmidt/httprouter"
+	"gopkg.in/mgo.v2"
+)
+
+type UserController struct {
+	session *mgo.Session
+}
+
+func NewUserController(s *mgo.Session) *UserController {
+	return &UserController{s}
+}
+
+// GetUser attempts to find a user based on a passed in Identification Document "ID"
+func (*UserController) GetUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	u := models.User{
+		Name:   "james bond",
+		Gender: "male",
+		Age:    32,
+		ID:     p.ByName("id"),
+	}
+
+	uj, err := json.Marshal(u)
+	if err != nil {
+		log.Println(err.Error())
+	}
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintf(w, "%s\n", uj)
+}
+
+// CreateUser makes a new user
+func (*UserController) CreateUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	u := models.User{}
+
+	// encode/decode for sending/receiving JSON to/from a stream
+	err := json.NewDecoder(r.Body).Decode(&u)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	u.ID = "007"
+
+	// marshal/unmarshal for having JSON assigned to variable
+	uj, err := json.Marshal(u)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated) // 201
+	fmt.Fprintf(w, "%s", uj)
+}
+
+// DeleteUser takes an ID and attempts to find if a user is present with that identifier
+// if found that user is removed
+func (*UserController) DeleteUser(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+	w.WriteHeader(http.StatusAccepted)
+	fmt.Fprint(w, "write code to delete user")
+}
