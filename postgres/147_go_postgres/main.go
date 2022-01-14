501f346c0d3a2c04d5306a2d32149ee9107a8db0
Salamandastron1 <tag.garrity@gmail.com>
2022-01-14 11:59:27 -0500

    connect to db

--- postgres/147_go_postgres/main.go
+++ postgres/147_go_postgres/main.go
@@ -3,22 +3,70 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"log"
+	"net/http"
 
 	_ "github.com/lib/pq"
 )
 
+type Book struct {
+	isbn   string
+	title  string
+	author string
+	price  float32
+}
+
+var db *sql.DB
+
+func init() {
+	var err error
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	// docker run -p 5432:5432 --name some-postgres -e POSTGRES_PASSWORD=password -d postgres
+	db, err = sql.Open("postgres", "postgres://bond:password@localhost/bookstore?sslmode=disable")
+	if err != nil {
+		panic(err)
+	}
+
+	if err = db.Ping(); err != nil {
+		panic(err)
+	}
+	fmt.Println("You connected to the DB")
+}
+
 func main() {
-	db, err := sql.Open("postgres", "postgres://bond:password@localhost/bookstore?sslmode=disable")
-	if err != nil {
-		panic(err)
-	}
-	// docker run -p 5432:5432 --name some-postgres -e POSTGRES_PASSWORD=mysecretpassword -d postgres
-	defer db.Close()
+	http.HandleFunc("/books", booksIndex)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
 
-	err = db.Ping()
+func booksIndex(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	rows, err := db.Query("SELECT * FROM books;")
 	if err != nil {
-		panic(err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+
+	bks := make([]Book, 0)
+	for rows.Next() {
+		bk := Book{}
+		err := rows.Scan(&bk.isbn, &bk.title, &bk.author, &bk.price)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		bks = append(bks, bk)
 	}
 
-	fmt.Println("You connected to your database.")
+	if err = rows.Err(); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	for _, bk := range bks {
+		fmt.Fprintf(w, "%s, %s, $%.2f\n", bk.isbn, bk.title, bk.price)
+	}
 }
